b3deeec3e08bdb21dc81ec7a28f32ced7948a04e
Triceraflops <nickhall@fastmail.net>
2017-01-01 12:35:16 +0100

    Problem 1 solved

--- problem1.py
+++ problem1.py
@@ -1,7 +1,7 @@
 ##Project Euler
 ##Problem number: 7
 ##Author: Nick Hall
-##Date: 31/12/2016
+##Date: 01/01/2017
 
 
 #Find the sum of multiples of 3 and 5 below 1000.
@@ -12,7 +12,9 @@
 threes = ((3 + 999) * (999 / 3)) / 2
 #Find sum of 5's below 1000 (995)
 fives = ((5 + 995) * (995 / 5)) / 2
-#Find sum of fives which are also multiples of 3
-doubles = 0
+#Find sum of fives which are also multiples of 3 (multiples of 15)
+fifteens = ((15 + 990) * (990 / 15) / 2)
 #Add the three's and five's, and subtract the doubles
-answer = threes + fives - doubles
+answer = threes + fives - fifteens
+
+print("The answer is: {}".format(answer))
