fcf33ceec169acb4068e620821dd195dac525bc8
Triceraflops <nickhall@fastmail.net>
2017-01-05 23:15:19 +0100

    problem 3 solved

--- problem3.py
+++ problem3.py
@@ -7,21 +7,33 @@
 
 #Using fundamental therom of arithmetic
 #Every number is either prime or a multiple of primes.
-#So finding the largest prime factor just means
+#So finding the largest prime factor just means breaking down the factors
+#as much as possible until they reach the primes, and taking the largest.
 
-#Imports
-#import numpy as np
 
 #Declare variables
-prime_list = np.array([2,3])
-prime_count = 2
-next_prime = 5
+number_to_test = 600851475143
+prime_factor = number_to_test
+divisor = 2
+result = 0 #not so neat, but needed because divisor gets written over as 2 on the last calculation
 
-#Prime finder loop
-while prime_count < 100000:
-    if (0 not in next_prime % prime_list):
-        prime_list = np.append(prime_list, next_prime)
-        prime_count += 1
-    next_prime += 2
+#Find factors, smallest will always be prime
+#Checks if the factor is evenly divisible by an odd number (divisor).
+#If divisble then use the remainder as the new number to test, and restart the divisor variable.
+while prime_factor > 1:
+    if prime_factor % divisor == 0:
+        prime_factor = prime_factor // divisor
+        result = divisor
+        divisor = 2
+    else:
+        ##increase by 2 (unless already 2)
+        if divisor == 2:
+            divisor += 1
+        else:
+            divisor += 2
 
-print("The 10001st prime is: {}".format(prime_list[-1]))
+print("Largest prime factor is: {}".format(result))
+
+##Output
+##Largest prime factor is: 6857
+##[Finished in 0.035s]
